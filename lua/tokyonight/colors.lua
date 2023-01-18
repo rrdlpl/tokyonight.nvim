4b9f3fdc445a365c42cafe27bba0f29e73158ab2
Roberto De la Parra <roberto.delaparra@ottonova.de>
2023-01-18 12:24:51 +0100

    testing changing keywords colors

--- lua/tokyonight/colors.lua
+++ lua/tokyonight/colors.lua
@@ -37,6 +37,8 @@ M.default = {
   brown = "#504945",
   white = "#f8f8f0",
   grey = "#8F908A",
+  pink = "#f92672",
+
   git = { change = "#27406b", add = "#3d5213", delete = "#4a0f23" },
   gitSigns = {
     add = "#3d5213",
